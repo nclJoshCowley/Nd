0e6830076ac9a624ce2c036e19c4c6dfc75e93c4
Josh Cowley <j.cowley1@ncl.ac.uk>
2023-03-25 19:08:44 +0000

    Added user-level Nd methods

---
+++ R/Nd-methods.R
@@ -0,0 +1,44 @@
+#' Check `Nd` Class
+#'
+#' @template Nd-method
+#'
+#' @export
+is.Nd <- function(x) {
+  inherits(x, "Nd")
+}
+
+
+#' Implementation of [pillar.type_sum]
+#'
+#' Changes type summary (shown in tibbles) from `Surv` to `Nd`.
+#'
+#' @template Nd-method
+#'
+#' @export
+type_sum.Nd <- function(x) {
+  "Nd"
+}
+
+
+#' Compute Degree of Censoring
+#'
+#' @template Nd-method
+#' @param na.rm logical. NA observations ignored when `TRUE`.
+#'
+#' @export
+cens_ratio <- function(x, na.rm = FALSE) {
+  mean(x$is_nd, na.rm = na.rm)
+}
+
+
+#' Visualise Non-Detect Data
+#'
+#' @template Nd-method
+#'
+#' @export
+geom_Nd <- function(x) {
+  stop(
+    "Not implemented. Requires better understanding of ggplot2.",
+    call. = FALSE
+  )
+}
