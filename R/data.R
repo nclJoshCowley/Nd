d366df83190153bf09e19d706972c90a4eac4b96
Josh Cowley <j.cowley1@ncl.ac.uk>
2023-03-27 18:36:41 +0100

    Added example Nd object

---
+++ R/data.R
@@ -0,0 +1,22 @@
+# Nd_example <- Nd(c(
+#   "<1", "<1", "<1", "<1", "<1", "<1",
+#   "3", "7", "9",
+#   "<10", "<10", "<10",
+#   "12", "15",  "20", "27", "33", "50"
+# ))
+# usethis::use_data(Nd_example)
+
+
+#' Helsel (1988) Example Data
+#'
+#' Used as toy example in Appendix B of Helsel and Cohn (1988).
+#'
+#' @format An object of class Nd (inherits from Surv) with 18 observations.
+#'
+#' @references
+#'   Helsel, D.R. and Cohn, T.A., 1988. Estimation of descriptive
+#'   statistics for multiply censored water quality data. Water Resources
+#'   Research, 24(12), pp.1997-2004.
+#'
+#' @source <https://doi.org/10.1029/WR024i012p01997>
+"Nd_example"
